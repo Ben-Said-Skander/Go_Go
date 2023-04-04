6e3a6892cd9404cffc7a1b9324f4f31eb0e8bf0c
Skander Ben Said <bensaid303@gmail.com>
2023-04-04 22:27:15 +0100

    link api

---
+++ lib/core/constants/link_api.dart
@@ -0,0 +1,17 @@
+class LinkApi {
+  static const server = "http://192.168.198.161:3600/";
+  static const medicines = "medicines";
+  static const register = "register";
+  static const user = "user";
+  static const blog = "blog";
+  static const images = "images";
+  static const image = "image";
+  static const auth = "auth";
+  static const pharmacies = "pharmacies";
+
+  static const updateEmail = "/updateEmail";
+  static const updatePassword = "/updatePassword";
+  static const updateName = "/updateName";
+  static const updatePhone = "/updatePhone";
+  static const forgotPassword = "/forgotPassword";
+}

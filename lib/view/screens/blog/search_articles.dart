349205b78ff3372c97890f3a82af6ffbee990df5
Skander Ben Said <bensaid303@gmail.com>
2023-04-05 21:49:16 +0100

    search filter

--- lib/view/screens/blog/search_articles.dart
+++ lib/view/screens/blog/search_articles.dart
@@ -1,30 +1,30 @@
 // ignore_for_file: unused_element, unused_local_variable
-import 'dart:convert';
 
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:pfa_application_1/controllers/blog_controller.dart';
 import 'package:pfa_application_1/core/constants/colors.dart';
+import 'package:pfa_application_1/core/constants/routes.dart';
 import 'package:pfa_application_1/models/blog.dart';
 import 'package:pfa_application_1/view/widgets/component/blog_card.dart';
 
 class SearchArticles extends StatefulWidget {
-  const SearchArticles({super.key});
+  const SearchArticles({Key? key}) : super(key: key);
 
   @override
-  State<SearchArticles> createState() => _SearchArticlesState();
+  _SearchArticlesState createState() => _SearchArticlesState();
 }
 
 class _SearchArticlesState extends State<SearchArticles> {
   BlogController blogController = Get.find();
-  List<Blog> articles = [];
+  Future<List<Blog>>? articles;
   List<Blog> filteredArticles = [];
+
+  int empty = 0;
+
   late TextEditingController searchController;
 
-  /* late final Future<List<Pharmacy>> futureLocations =
-      pharmacyController.getAllPharmacies();*/
   @override
   void dispose() {
     searchController.dispose();
@@ -33,12 +33,7 @@ class _SearchArticlesState extends State<SearchArticles> {
 
   @override
   void initState() {
-    fetchArticles().then((articleFromServer) {
-      setState(() {
-        articles = articleFromServer;
-        filteredArticles = articles;
-      });
-    });
+    articles = blogController.fetchArticles();
     searchController = TextEditingController();
     super.initState();
   }
@@ -46,91 +41,112 @@ class _SearchArticlesState extends State<SearchArticles> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-              padding: const EdgeInsets.only(top: 60, left: 5, right: 30),
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Get.back();
-                    },
-                    icon: Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.black,
-                    ),
-                  ),
-                  Text("Search for article",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: AppColor.mainColor,
-                          fontSize: 20,
-                          fontFamily: "Poppins")),
-                ],
-              )),
-          Container(
-            padding: EdgeInsets.only(top: 40, left: 20),
-            width: 400,
-            child: TextFormField(
-              onChanged: (titleEntered) {
-                filteredArticles = articles
-                    .where((article) => article.title!
-                        .toLowerCase()
-                        .contains(titleEntered.toLowerCase()))
-                    .toList();
-              },
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                hintText: "Search for an article",
-                hintStyle: TextStyle(color: Colors.black),
-                prefixIcon: Icon(FontAwesomeIcons.magnifyingGlass,
-                    color: AppColor.mainColor),
-                enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.circular(30)),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: AppColor.mainColor),
-                    borderRadius: BorderRadius.circular(30)),
+        body: ListView(children: [
+      Padding(
+          padding: const EdgeInsets.only(top: 15, left: 5, right: 30),
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Get.back();
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.black,
+                ),
               ),
-              controller: searchController,
-              cursorColor: Color.fromARGB(255, 16, 152, 170),
-            ),
+              Text("Search for article",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColor.mainColor,
+                      fontSize: 20,
+                      fontFamily: "Poppins")),
+            ],
+          )),
+      Container(
+        padding: EdgeInsets.only(top: 40, left: 20),
+        width: 400,
+        child: TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+            hintText: "Search for an article",
+            hintStyle: TextStyle(color: Colors.black),
+            prefixIcon: Icon(FontAwesomeIcons.magnifyingGlass,
+                color: AppColor.mainColor),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: AppColor.mainColor),
+                borderRadius: BorderRadius.circular(30)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: AppColor.mainColor),
+                borderRadius: BorderRadius.circular(30)),
           ),
-          SizedBox(
-            height: 40,
-          ),
-          GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2),
-              itemCount: filteredArticles.length,
-              itemBuilder: ((context, index) {
-                return BlogCard(
-                    blogTitle: filteredArticles[index].title!,
-                    blogPicture: 'blogPicture');
-              }))
-        ],
+          controller: searchController,
+          cursorColor: Color.fromARGB(255, 16, 152, 170),
+        ),
       ),
-    );
-  }
-}
-
-Future<List<Blog>> fetchArticles() async {
-  List<Blog> totalArticles = [];
-  final response =
-      await http.get(Uri.parse('http://192.168.101.161:3600/blog'));
-
-  if (response.statusCode == 200) {
-    final parsed = jsonDecode(response.body).cast<Map<String, dynamic>>();
-    totalArticles = parsed.map<Blog>((e) => Blog.fromJson(e)).toList();
-    ;
-    print(totalArticles);
-    return totalArticles;
-  } else {
-    throw Exception('Failed to load Articles');
+      SizedBox(
+        height: 15,
+      ),
+      Container(
+          child: FutureBuilder<List<Blog>>(
+        future: blogController.fetchArticles(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Padding(
+                padding: EdgeInsets.only(left: 100, right: 100, top: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: AppColor.mainColor),
+                  child: MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        filteredArticles = snapshot.data!
+                            .where((article) =>
+                                article.title == searchController.text)
+                            .toList();
+                      });
+                    },
+                    child:
+                        Text("Search", style: TextStyle(color: Colors.white)),
+                  ),
+                ));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      )),
+      SizedBox(
+        height: 40,
+      ),
+      if (filteredArticles.isNotEmpty)
+        Container(
+            child: GridView.builder(
+          shrinkWrap: true,
+          physics: ClampingScrollPhysics(),
+          itemCount: filteredArticles.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: ((context, index) {
+            return GestureDetector(
+              onTap: () {
+                var articleId = "${filteredArticles[index].id}";
+                var imageId = "${filteredArticles[index].imageId}";
+                Get.toNamed(AppRoute.blogdetails, arguments: {
+                  'articleId': '${articleId}',
+                  'imageId': '${imageId}'
+                });
+              },
+              child: BlogCard(
+                blogTitle: "${filteredArticles[index].title}",
+                blogPicture: "assets/image/hand2.jpg",
+              ),
+            );
+          }),
+        ))
+    ]));
   }
 }
